b6f063d40ae09d80c5e2c33ab45ef52bad34eace
Developer <dev@local>
2026-05-12 17:51:01 +0800

    feat: wire AppDelegate to handle NSServices with ActionDispatcher

--- mac-right-helper/AppDelegate.swift
+++ mac-right-helper/AppDelegate.swift
@@ -2,7 +2,28 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusBarController: StatusBarController?
+
     func applicationDidFinishLaunching(_ notification: Notification) {
-        print("mac-right-helper launched")
+        statusBarController = StatusBarController()
+        NSUpdateDynamicServices()
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(configChanged),
+            name: ConfigManager.configChangedNotification,
+            object: nil
+        )
+    }
+
+    func handleService(_ pboard: NSPasteboard, userData: String, error: AutoreleasingUnsafeMutablePointer<NSString>) {
+        let paths = PasteboardReader.extractFilePaths(from: pboard)
+        Task {
+            await ActionDispatcher.dispatch(actionID: userData, filePaths: paths)
+        }
+    }
+
+    @objc private func configChanged() {
+        NSUpdateDynamicServices()
     }
 }
