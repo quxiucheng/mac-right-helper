241bf4fba1a01966a7cf03985896ae8105af2d46
Developer <dev@local>
2026-05-12 17:38:53 +0800

    scaffold: create project structure and base files

---
+++ mac-right-helper/AppDelegate.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        print("mac-right-helper launched")
+    }
+}
